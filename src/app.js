7eb51563a0ac5eaaa93a0ac830266249596e7afb
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2023-01-10 08:05:28 -0600

    Endpoint JR

--- src/app.js
+++ src/app.js
@@ -7,4 +7,9 @@ app.get('/',(req,res)=>{
     res.send("Inicio de mi servidor");
 })
 
+app.get('/endpointjunior',(req,res)=>{
+    res.send("Endpoint hecho por Mau JR")
+})
+
+
 app.listen(8080,()=>console.log("Listening"))
